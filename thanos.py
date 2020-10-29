4145acd221d5b3952f76f9a92b681170443d0f3b
JKippy <kaipjason@gmail.com>
2020-10-29 12:59:57 -0400

    Convoluted

--- thanos.py
+++ thanos.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
 import os, random
+
 files = len([name for name in os.listdir('.') if os.path.isfile(name)])
 names = [name for name in os.listdir('.') if os.path.isfile(name)]
+
+print names
 half = files/2
 while half > 0:
 	ran = random.randint(0,files-1)
-	if names[ran] != "thanos.py":
+	if names[ran] != "thanos.py" and names[ran] != "thanosvid.mp4":
 		if os.path.exists(names[ran]):
 			print "Removing: " + names[ran]
+			os.remove(names[ran])
 			half = half-1
 print "Stopped"
